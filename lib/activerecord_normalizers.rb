0b83eb695626523dc47b9429cef192ce677b992e
Michael Josephson <30024+mikej@users.noreply.github.com>
2026-02-18 13:26:53 +0000

    Initial commit

---
+++ lib/activerecord_normalizers.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "activerecord_normalizers/version"
+
+module ActiverecordNormalizers
+  class Error < StandardError; end
+  # Your code goes here...
+end
